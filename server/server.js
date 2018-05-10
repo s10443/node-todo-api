c58fe07cda011325387b9ffee3503dd0b72a137c
s10443 <104703043@nccu.edu.tw>
2018-05-10 10:21:29 +0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,3 +24,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app};
